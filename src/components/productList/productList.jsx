a2ae9e8bc611f6e98097fe63d281c1d124b32668
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2023-11-28 00:54:14 +0100

    Poprawka

--- src/components/productList/productList.jsx
+++ src/components/productList/productList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./productList.scss";
 import { useProducts } from "../../data/productsContext";
 
