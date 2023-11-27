3986cd0118c35e6c71b795b1461608b09904aa3d
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2023-11-28 00:56:46 +0100

    CHYBA WSZYTYSTKO

--- src/components/addNewProduct/addNewProduct.jsx
+++ src/components/addNewProduct/addNewProduct.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./addNewProduct.scss";
 
 import { useProducts } from "../../data/productsContext";
@@ -32,38 +31,3 @@ export function AddNewProduct() {
 		</div>
 	);
 }
-=======
-import "./addNewProduct.scss";
-
-import { useProducts } from "../../data/productsContext";
-
-export function AddNewProduct() {
-	const { addProduct } = useProducts();
-
-	const handleKeyPress = (event) => {
-		if (event.key === "Enter") {
-			if (event.target.value) {
-				// Tutaj wykonaj dowolne działania, które mają być wykonane po naciśnięciu Enter
-				addProduct(event.target.value);
-				// console.log("Dodawanie prodktu o kodzie: ", event.target.value);
-				event.target.value = "";
-			}
-		}
-	};
-
-	return (
-		<div className="add_new_product">
-			<label htmlFor="new_product">
-				<h5>Dodaj</h5>
-				<input
-					type="text"
-					id="new_product"
-					name="new_product"
-					autoComplete="off"
-					onKeyDown={handleKeyPress}
-				/>
-			</label>
-		</div>
-	);
-}
->>>>>>> 8db75f9c2fcc7766c7e77cd188b6fba6b6b68574
